512960c86d0dddb5ccfba8889b1a6dbe0fe9afc0
Gosse <thms.gosse@gmail.com>
2018-12-24 00:58:45 +0100

    feat(discogs): create and plug list component to display search results

--- src/components/Search/SearchBarContainer.vue
+++ src/components/Search/SearchBarContainer.vue
@@ -1,14 +1,54 @@
 <template>
-  <SearchBar/>
+  <SearchBar
+    :on-change="search"
+  >
+    <SearchBarList
+      :items="items"
+      :on-click="onClick"
+    />
+  </SearchBar>
 </template>
 
 <script>
+import UIkit from 'uikit';
 import SearchBar from '@/components/Search/SearchBar';
+import SearchBarList from '@/components/Search/SearchBarList';
+import { getSearchResult } from '@/services/discogs';
+
+const defaultImage = require('@/assets/logo.png');
 
 export default {
   name: 'SearchBarContainer',
   components: {
     SearchBar,
+    SearchBarList,
+  },
+  data() {
+    return {
+      items: [],
+    };
+  },
+  methods: {
+    hasImage(uri) {
+      return uri !== 'https://img.discogs.com/images/spacer.gif';
+    },
+    search(search) {
+      if (search.length > 1) {
+        getSearchResult(search)
+          .then((response) => {
+            const results = response.results;
+            this.items = results.map(item => ({
+              ...item,
+              cover_image: (this.hasImage(item.cover_image)) ? item.cover_image : defaultImage,
+            }));
+          });
+      }
+      return [];
+    },
+    onClick() {
+      UIkit.offcanvas('#offcanvas-push').toggle();
+      this.$router.push('/');
+    },
   },
 };
 </script>
---
+++ src/components/Search/SearchBarList.vue
@@ -0,0 +1,56 @@
+<template>
+  <table class="uk-table">
+    <tbody>
+      <tr
+        v-for="item in items"
+        :key="item.id"
+      >
+        <td
+          uk-grid
+          class="uk-grid-small uk-child-width-expand@s uk-text-left"
+          @click="onClick">
+          <div class="uk-width-1-3@m">
+            <img
+              :data-src="item.cover_image"
+              uk-img
+              width="70"
+              height="70"
+            >
+          </div>
+          <div class="uk-width-2-3@m">
+            <div>
+              {{ item.title }}
+            </div>
+            <div class="uk-text-meta">
+              {{ item.type }}
+            </div>
+
+          </div>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'SearchBarList',
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+    onClick: {
+      type: Function,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+.uk-text-meta {
+  text-transform: capitalize
+}
+</style>
+

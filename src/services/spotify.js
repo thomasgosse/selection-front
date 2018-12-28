dfaf9de6326429cf768cb74d216f7d9724095b56
Gosse <thms.gosse@gmail.com>
2018-12-28 23:49:18 +0100

    feat(spotify): change discogs service to spotify

--- src/services/spotify.js
+++ src/services/spotify.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const token = 'Bearer '.concat('BQDYtt0LaT7dsi9wkwpE_IPUvu7uD9w5whdlGv95c9JcpWItC3mbKazayDHeozTYqUFCJFIJCqOduEoVQiU');
+const token = 'Bearer '.concat('BQBKO0h9f-Qjan6Z5Nf6_T3iGQImPyEnDR741TyA6jNP4UnyGiRdk6814SmA67BVnqN6cO5ZNbj2QnuNehI');
 
 export async function getSearchResult(search) {
   return axios

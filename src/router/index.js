45d893c54b79662f901e48da9e3d39bcd7232bc1
Gosse <thms.gosse@gmail.com>
2018-12-28 23:50:41 +0100

    feat(router): add auth component in router

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Main from '@/components/Main';
+import AuthContainer from '@/components/AuthContainer';
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'Main',
       component: Main,
+      props: true,
+    },
+    {
+      path: '/callback',
+      name: 'AuthContainer',
+      component: AuthContainer,
     },
   ],
 });

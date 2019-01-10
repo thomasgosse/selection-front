51cd73c1bccd5fbb2aa915bda97527c88384f223
Gosse <thms.gosse@gmail.com>
2019-01-10 14:58:51 +0100

    feat(interceptors): create interceptor and use them to catch error 401

--- src/main.js
+++ src/main.js
@@ -4,14 +4,19 @@ import App from './App';
 import router from './router';
 import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
+import store from './store/store';
+import interceptors from './services/interceptors';
 
 UIkit.use(Icons);
 
 Vue.config.productionTip = false;
 
-new Vue({
+interceptors.setup(router, store);
+
+const app = new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>',
 });
---
+++ src/services/interceptors.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import UIkit from 'uikit';
+
+export default {
+  setup: (router, store) => {
+    axios.interceptors.response.use(
+      response => response,
+      (error) => {
+        if (error.response.data.error.message === 'The access token expired') {
+          console.log('expired token');
+        }
+        UIkit.offcanvas('#offcanvas-push').hide();
+        store.commit('login', false);
+      },
+    );
+  },
+};

f050aa14858a660b7bb358ec810d0988b4c52755
Gosse <thms.gosse@gmail.com>
2019-01-10 17:05:26 +0100

    chore(icons): add fontawesome to project

--- src/main.js
+++ src/main.js
@@ -6,6 +6,13 @@ import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
 import store from './store/store';
 import interceptors from './services/interceptors';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSpotify } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faSpotify)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 UIkit.use(Icons);
 
